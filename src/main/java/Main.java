3a47f2fbac7c78590a7aaac721eaba8436972f81
Paweł Stefanowski <paw.stefanowski@gmail.com>
2019-06-04 20:18:47 +0200

    gitIgnor

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,11 @@
 public class Main {
     public static void main(String[] args) {
 
-        
+        Student student1 = new Student(123467,"Leszek","Możdzer");
+        Student student2 = new Student(122212, "sdf","adsdfa");
+        Student student3 = new Student(23134, "sdfsergs","wwwww");
+        Student student4 = new Student(5346567, "sfafsa", "ksdygbdh");
+
 
     }
 }
---
+++ src/main/java/Student.java
@@ -0,0 +1,12 @@
+import lombok.AllArgsConstructor;
+import lombok.Data;
+@Data
+@AllArgsConstructor
+public class Student {
+
+    private long numerIndeksu;
+    private String imie;
+    private String nazwisko;
+
+
+}
