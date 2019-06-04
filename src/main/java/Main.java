c520d9855d2fdb5e52b43a76600c18187cce0857
Paweł Stefanowski <paw.stefanowski@gmail.com>
2019-06-04 20:24:51 +0200

    coś poszło nie tak

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,6 +5,7 @@ public class Main {
         Student student2 = new Student(122212, "sdf","adsdfa");
         Student student3 = new Student(23134, "sdfsergs","wwwww");
         Student student4 = new Student(5346567, "sfafsa", "ksdygbdh");
+        System.out.println(student1);
 
 
     }
